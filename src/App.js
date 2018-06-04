bcde20e5403cd4e0c6e76ed2286f6423dcafe797
Sadathe Toure <trms75@gmail.com>
2018-06-04 16:36:35 -0600

    Initial push

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ul className="App">
+            <li className='boy'>Name 2</li>
+            <li className='girl'>Name 2</li>
+            <li className='boy'>Name 3</li>
+            <li className='girl'>Name 3</li>
+            <li className='boy'>Name 2</li>
+            <li className='girl'>Name 2</li>
+            <li className='boy'>Name 3</li>
+            <li className='boy'>Name 3</li>
+      </ul>
+    );
+  }
+}
+
+export default App;
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
